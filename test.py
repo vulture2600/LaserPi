f13eaa52bdf288dcde68cdf049a02e6ddd9133fc
vulture2600 <steve.a.mccluskey@gmail.com>
2023-07-27 18:21:09 -0500

    adding some example code

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ buzzer             = 22
 
 
 import RPi.GPIO as GPIO
+import pigpio
 import time
 
 GPIO.setmode(GPIO.BCM)
@@ -41,24 +42,32 @@ GPIO.setup(panServoPin, GPIO.OUT)
 
 
 #converts angle parameters to PWM:
-
+def degreesToPwm(degree):
+    return int((degree) * (2000) / (180) + 500)
 
 
 
 #test  servos:
+#useful duty cycle range seems to be between 3 and 10 duty cycle
 panServo = GPIO.PWM(panServoPin, 50)
-panServo.start(2.5)
+panServo.start(5)
 tiltServo = GPIO.PWM(tiltServoPin, 50)
-tiltServo.start(2.5)
+tiltServo.start(5)
+print("start")
+time.sleep(5)
+
 try:
     while True:
-        panServo.ChangeDutyCycle(5)
-        tiltServo.ChangeDutyCycle(5)
-        time.sleep(.5)
-        panServo.ChangeDutyCycle(16)
-        tiltServo.ChangeDutyCycle(16)
-        time.sleep(.5)
+        panServo.ChangeDutyCycle(3)
+        tiltServo.ChangeDutyCycle(3)
+        print("duty cycle low range.")
+        time.sleep(2)
+        panServo.ChangeDutyCycle(10)
+        tiltServo.ChangeDutyCycle(10)
+        print("cuty cycle high range.")
+        time.sleep(2)
 except KeyboardInterrupt:
+
     panServo.stop
     tiltServo.stop
     GPIO.cleanup()        
